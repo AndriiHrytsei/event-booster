e46c78e27fac5b61c4c5ac192cb1a7a60e1f2870
Andrii Hrytsei <andrii.hrytsei@gmail.com>
2023-05-07 19:44:56 +0300

    api practice

---
+++ src/js/index.js
@@ -0,0 +1,27 @@
+const eventList = document.querySelector(".list-cards")
+
+async function fetchEvents(api){
+    const response = await fetch(api)
+    const events = await response.json()
+    return events
+}
+
+fetchEvents("https://app.ticketmaster.com/discovery/v2/events.json?size=20&apikey=Thqn5txrZvBNrP2vPhyOGtn3h4ymZ92S")
+    .then(data => {
+        renderEvents(data["_embedded"]["events"])
+        console.log(data["_embedded"]["events"])
+    })
+
+function renderEvents(events) {
+    events.forEach(event =>{
+        eventList.insertAdjacentHTML("beforeend", `
+            <li class="event-1 event">
+                <div class="ramka-event"></div>
+                <img src=${event["images"][0]["url"]}>
+                <h3>${event["name"]}</h3>
+                <h4>${event["dates"]["start"]["localDate"]}</h4>
+                <p>${event["_embedded"]["venues"][0]["name"]}</p>
+            </li>
+        `)
+    })
+}
