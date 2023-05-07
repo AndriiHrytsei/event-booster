ebbd5e782817b6a32e527885bbfd81f3066195f7
Andrii Hrytsei <andrii.hrytsei@gmail.com>
2023-05-07 20:22:11 +0300

    add api events and minor fix

--- src/js/index.js
+++ src/js/index.js
@@ -1,33 +1,11 @@
-import * as modalsJS from 'modals.js'
+import * as modalsJS from './modals.js'
+import * as eventsJS from './events.js'
 
-const eventList = document.querySelector(".list-cards")
-
-async function fetchEvents(api){
-    const response = await fetch(api)
-    const events = await response.json()
-    return events
-}
-
-fetchEvents("https://app.ticketmaster.com/discovery/v2/events.json?size=20&apikey=Thqn5txrZvBNrP2vPhyOGtn3h4ymZ92S")
+eventsJS.fetchEvents("https://app.ticketmaster.com/discovery/v2/events.json?size=20&apikey=Thqn5txrZvBNrP2vPhyOGtn3h4ymZ92S")
     .then(data => {
-        renderEvents(data["_embedded"]["events"])
+        eventsJS.renderEvents(data["_embedded"]["events"])
         console.log(data["_embedded"]["events"])
     })
-
-function renderEvents(events) {
-    events.forEach(event =>{
-        eventList.insertAdjacentHTML("beforeend", `
-            <li class="event-1 event">
-                <div class="ramka-event"></div>
-                <img src=${event["images"][0]["url"]}>
-                <h3>${event["name"]}</h3>
-                <h4>${event["dates"]["start"]["localDate"]}</h4>
-                <p>${event["_embedded"]["venues"][0]["name"]}</p>
-            </li>
-        `)
-    })
-}
-
 
 
 modalsJS.span.addEventListener("click", () => {
