4e62f3f4d7db2e4d49b6eb57af95367aecc9dceb
Andrii Hrytsei <andrii.hrytsei@gmail.com>
2023-05-14 13:24:05 +0300

    info about event in modal

--- src/js/modals.js
+++ src/js/modals.js
@@ -1,6 +1,20 @@
 export const modal = document.querySelector("#myModal");
 export const span = document.querySelector(".svg-close");
-// export const cardEvents = document.querySelectorAll(".event")
+
+//! Modal elements
+export const eventImgHead = document.querySelector(".event-image-head")
+export const eventImgMain = document.querySelector(".main-event-image")
+export const info = document.querySelector(".info-text")
+export const date = document.querySelector(".date")
+export const time = document.querySelector(".time")
+export const location = document.querySelector(".location")
+export const place = document.querySelector(".place")
+export const who = document.querySelector(".who-name")
+export const standartPrice = document.querySelector(".standart-price")
+export const vipPrice = document.querySelector(".vip-price")
+
+
+
 
 // span.addEventListener("click", () => {
 //   modal.style.display = "none";
